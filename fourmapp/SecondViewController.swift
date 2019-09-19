2bd5eaef0e95aeb6ac78ab280085e3efdf4d54b7
RAY <raywnwa@gmail.com>
2019-09-19 22:33:43 +0800

    Initial Commit

---
+++ fourmapp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fourmapp
+//
+//  Created by KA CHON HO on 19/09/2019.
+//  Copyright © 2019 KA CHON HO. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
