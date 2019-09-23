dbbfa820b0e7bb5b22351ac5b53e0513a78a50db
RAY <raywnwa@gmail.com>
2019-09-23 23:08:10 +0800

    tab bar item

---
+++ MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  fourmapp
+//
+//  Created by KA CHON HO on 20/09/2019.
+//  Copyright © 2019 KA CHON HO. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
